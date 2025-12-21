4fcaf02305bd2a17c31805f7e34a93aa55966f8e
DIVINEHOPE9 <fynefacedivine52@gmail.com>
2025-12-21 19:31:24 +0100

    Create product.js

---
+++ js/product.js
@@ -0,0 +1,14 @@
+const products = [
+  {
+    id: 1,
+    name: "Smart Watch",
+    price: 8500,
+    image: "images/products/watch.jpg"
+  },
+  {
+    id: 2,
+    name: "Wireless Earbuds",
+    price: 6500,
+    image: "images/products/earbuds.jpg"
+  }
+];
