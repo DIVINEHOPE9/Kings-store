0c2d7f5c15cd5ff9d75efbe0896577447b2c7ed8
DIVINEHOPE9 <fynefacedivine52@gmail.com>
2025-12-21 21:07:10 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,38 +1,22 @@
-const productList = document.getElementById("product-list");
+document.addEventListener("DOMContentLoaded", function () {
+  const productList = document.getElementById("product-list");
 
-function displayProducts(list) {
-  productList.innerHTML = "";
+  if (!productList) {
+    alert("Product list not found");
+    return;
+  }
 
-  list.forEach(product => {
+  products.forEach(product => {
     const div = document.createElement("div");
     div.className = "product";
 
     div.innerHTML = `
       <img src="${product.image}">
       <h3>${product.name}</h3>
-      <p>₦${product.price.toLocaleString()}</p>
-      <button onclick="addToCart(${product.id})">Add to Cart</button>
+      <p>₦${product.price}</p>
+      <button>Add to Cart</button>
     `;
 
     productList.appendChild(div);
   });
-}
-
-function addToCart(id) {
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
-  const product = products.find(p => p.id === id);
-  cart.push(product);
-  localStorage.setItem("cart", JSON.stringify(cart));
-  alert("Added to cart 🛒");
-}
-
-function filterCategory(cat) {
-  if (cat === "all") {
-    displayProducts(products);
-  } else {
-    const filtered = products.filter(p => p.category === cat);
-    displayProducts(filtered);
-  }
-}
-
-displayProducts(products);
+});
